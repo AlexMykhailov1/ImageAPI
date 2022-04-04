8637b9c422a024a73f7fa36af2b40e515850b7d0
alex <anonim2771@gmail.com>
2022-04-04 15:05:14 +0300

    Add config file

---
+++ config/config.go
@@ -0,0 +1,48 @@
+package config
+
+import "github.com/spf13/viper"
+
+// Config stores all configuration of the app
+type Config struct {
+	DB
+	SRV
+}
+
+// DB stores configuration for database
+type DB struct {
+	Host     string `mapstructure:"DB_HOST"`
+	Port     string `mapstructure:"DB_PORT"`
+	User     string `mapstructure:"DB_USER"`
+	Password string `mapstructure:"DB_PASSWORD"`
+	Name     string `mapstructure:"DB_NAME"`
+	SSL      string `mapstructure:"DB_SSL_MODE"`
+}
+
+// SRV stores configuration for the server
+type SRV struct {
+	Port string `mapstructure:"SERVER_PORT"`
+}
+
+// LoadConfig reads configuration from file or environment variables
+func LoadConfig(path string) (*Config, error) {
+	cfg := new(Config)
+
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	// Load config
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, err
+	}
+	// Unmarshal variables
+	if err := viper.Unmarshal(&cfg.DB); err != nil {
+		return nil, err
+	}
+	if err := viper.Unmarshal(&cfg.SRV); err != nil {
+		return nil, err
+	}
+	return cfg, nil
+}
