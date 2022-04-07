5a53186d8db6fbebd0e7c4d3c7b9bb0fa75e11ab
alex <anonim2771@gmail.com>
2022-04-07 16:36:06 +0300

    Add RabbitMQ configuration

--- config/config.go
+++ config/config.go
@@ -4,9 +4,10 @@ import "github.com/spf13/viper"
 
 // Config stores all configuration of the app
 type Config struct {
-	DB
-	SRV
-	Path
+	DB   DB
+	SRV  SRV
+	Path Path
+	RB   RB
 }
 
 // DB stores configuration for database
@@ -27,6 +28,15 @@ type SRV struct {
 // Path stores path environment variables
 type Path struct {
 	Img string `mapstructure:"IMG_PATH"`
+}
+
+// RB stores configuration for the RabbitMQ
+type RB struct {
+	Host     string `mapstructure:"RB_HOST"`
+	Port     string `mapstructure:"RB_PORT"`
+	User     string `mapstructure:"RB_USER"`
+	Password string `mapstructure:"RB_PASSWORD"`
+	ImgQueue string `mapstructure:"QUEUE_NAME_IMG"`
 }
 
 // LoadConfig reads configuration from file or environment variables
@@ -51,6 +61,9 @@ func LoadConfig(path string) (*Config, error) {
 		return nil, err
 	}
 	if err := viper.Unmarshal(&cfg.Path); err != nil {
+		return nil, err
+	}
+	if err := viper.Unmarshal(&cfg.RB); err != nil {
 		return nil, err
 	}
 	return cfg, nil
